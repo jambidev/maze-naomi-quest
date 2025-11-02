cf96b387ceda054572c4c2ef05f53dfb9497ddd8
Mardianto Eka Saputra, S.Kom <98078917+jambidev@users.noreply.github.com>
2025-11-02 08:46:49 +0700

    Delete supabase directory

--- supabase/migrations/20251030115931_3354ce97-2437-4545-82ef-bd8d832f69fc.sql
+++
@@ -1,7 +0,0 @@
--- Add foreign key from game_scores to profiles
-ALTER TABLE public.game_scores 
-  DROP CONSTRAINT IF EXISTS game_scores_user_id_fkey,
-  ADD CONSTRAINT game_scores_user_id_fkey 
-    FOREIGN KEY (user_id) 
-    REFERENCES public.profiles(id) 
-    ON DELETE CASCADE;
